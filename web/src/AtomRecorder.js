bca6b0c3188a76842a7ef7a35e1073e16692faf1
neurospeech <ackava@gmail.com>
2014-07-21 19:41:42 +0530

    Attempting KeyboardEvent

--- web/src/AtomRecorder.js
+++ web/src/AtomRecorder.js
@@ -1,5 +1,182 @@
 ﻿/// <reference path="../Scripts/jquery-1.11.1.js" />
 
+
+(function(window) {//closure
+ 
+    var global = window;
+
+    var own = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
+
+    var _Object_defineProperty = Object.defineProperty || function (obj, prop, val) {
+        if ("value" in val) {
+            obj[prop] = val.value;
+        }
+    };
+
+    var _initKeyboardEvent_type = (function (e)
+    {
+        try {
+            e.initKeyboardEvent(
+				"keyup" // in DOMString typeArg
+				, true // in boolean canBubbleArg
+				, true // in boolean cancelableArg
+				, global // in views::AbstractView viewArg
+				, "+" // [test]in DOMString keyIdentifierArg | webkit event.keyIdentifier | IE9 event.key
+				, 3 // [test]in unsigned long keyLocationArg | webkit event.keyIdentifier | IE9 event.location
+				, true // [test]in boolean ctrlKeyArg | webkit event.shiftKey | old webkit event.ctrlKey | IE9 event.modifiersList
+				, false // [test]shift | alt
+				, true // [test]shift | alt
+				, false // meta
+				, false // altGraphKey
+			);
+			
+			
+			
+            // Safari and IE9 throw Error here due keyCode, charCode and which is readonly
+            // Uncomment this code block if you need legacy properties
+            delete e.keyCode;
+            _Object_defineProperty(e, "keyCode", { writable: true, configurable: true, value: 9 });
+            delete e.charCode;
+            _Object_defineProperty(e, "charCode", { writable: true, configurable: true, value: 9 });
+            delete e.which;
+            _Object_defineProperty(e, "which", { writable: true, configurable: true, value: 9 });
+			
+            return ((e.keyIdentifier || e.key) === "+" && (e.Location) === 3) && (
+				e.ctrlKey ?
+					e.altKey ? // webkit
+						1
+						:
+						3
+					:
+					e.shiftKey ?
+						2 // webkit
+						:
+						4 // IE9
+				) || 9 // FireFox|w3c
+            ;
+        }
+        catch (__e__) { _initKeyboardEvent_type = 0; }
+    })(document.createEvent( "KeyboardEvent" ));
+ 
+    var _keyboardEvent_properties_dictionary = {
+        "char": "",
+        "key": "",
+        "location": 0,
+        "ctrlKey": false,
+        "shiftKey": false,
+        "altKey": false,
+        "metaKey": false,
+        "repeat": false,
+        "locale": "",
+ 
+        "detail": 0,
+        "bubbles": false,
+        "cancelable": false,
+		
+        //legacy properties
+        "keyCode": 0,
+        "charCode": 0,
+        "which": 0
+    };
+ 
+ 
+    function crossBrowser_initKeyboardEvent(type, dict) {
+	var e;
+	if( _initKeyboardEvent_type ) {
+		e = document.createEvent( "KeyboardEvent" );
+	}else {
+		e = document.createEvent( "Event" );
+	}
+	var _prop_name
+		, localDict = {};
+ 
+	for( _prop_name in _keyboardEvent_properties_dictionary ) if(own(_keyboardEvent_properties_dictionary, _prop_name) ) {
+		localDict[_prop_name] = (own(dict, _prop_name) && dict || _keyboardEvent_properties_dictionary)[_prop_name];
+	}
+ 
+	var _ctrlKey = localDict.ctrlKey
+		, _shiftKey = localDict.shiftKey
+		, _altKey = localDict.altKey
+		, _metaKey = localDict.metaKey
+		, _altGraphKey = localDict.altGraphKey
+ 
+		, _modifiersListArg = _initKeyboardEvent_type > 3 ? (
+			(_ctrlKey ? "Control" : "")
+				+ (_shiftKey ? " Shift" : "")
+				+ (_altKey ? " Alt" : "")
+				+ (_metaKey ? " Meta" : "")
+				+ (_altGraphKey ? " AltGraph" : "")
+			).trim() : null
+ 
+		, _key = localDict.key 
+		, _char = localDict.char 
+		, _location = localDict.location
+		, _keyCode = localDict.keyCode || (localDict.keyCode = _key && _key.charCodeAt( 0 ) || 0)
+		, _charCode = localDict.charCode || (localDict.charCode = _char && _char.charCodeAt( 0 ) || 0)
+ 
+		, _bubbles = localDict.bubbles
+		, _cancelable = localDict.cancelable
+ 
+		, _repeat = localDict.repeat
+		, _locale = localDict.locale
+		, _view = global
+	;
+	
+	localDict.which || (localDict.which = localDict.keyCode);
+ 
+	if( "initKeyEvent" in e ) {//FF
+		//https://developer.mozilla.org/en/DOM/event.initKeyEvent
+		e.initKeyEvent( type, _bubbles, _cancelable, _view, _ctrlKey, _altKey, _shiftKey, _metaKey, _keyCode, _charCode );
+	}
+	else if(  _initKeyboardEvent_type && "initKeyboardEvent" in e ) {//https://developer.mozilla.org/en/DOM/KeyboardEvent#initKeyboardEvent()
+		if( _initKeyboardEvent_type === 1 ) { // webkit
+			//http://stackoverflow.com/a/8490774/1437207
+			//https://bugs.webkit.org/show_bug.cgi?id=13368
+			e.initKeyboardEvent( type, _bubbles, _cancelable, _view, _key, _location, _ctrlKey, _shiftKey, _altKey, _metaKey, _altGraphKey );
+		}
+		else if( _initKeyboardEvent_type === 2 ) { // old webkit
+			//http://code.google.com/p/chromium/issues/detail?id=52408
+			e.initKeyboardEvent( type, _bubbles, _cancelable, _view, _ctrlKey, _altKey, _shiftKey, _metaKey, _keyCode, _charCode );
+		}
+		else if( _initKeyboardEvent_type === 3 ) { // webkit
+			e.initKeyboardEvent( type, _bubbles, _cancelable, _view, _key, _location, _ctrlKey, _altKey, _shiftKey, _metaKey, _altGraphKey );
+		}
+		else if( _initKeyboardEvent_type === 4 ) { // IE9
+			//http://msdn.microsoft.com/en-us/library/ie/ff975297(v=vs.85).aspx
+			e.initKeyboardEvent( type, _bubbles, _cancelable, _view, _key, _location, _modifiersListArg, _repeat, _locale );
+		}
+		else { // FireFox|w3c
+			//http://www.w3.org/TR/DOM-Level-3-Events/#events-KeyboardEvent-initKeyboardEvent
+			//https://developer.mozilla.org/en/DOM/KeyboardEvent#initKeyboardEvent()
+			e.initKeyboardEvent( type, _bubbles, _cancelable, _view, _char, _key, _location, _modifiersListArg, _repeat, _locale );
+		}
+	}
+	else {
+	    e.initEvent(type, _bubbles, _cancelable);
+	}
+ 
+	for( _prop_name in _keyboardEvent_properties_dictionary )if( own( _keyboardEvent_properties_dictionary, _prop_name ) ) {
+		if( e[_prop_name] !== localDict[_prop_name] ) {
+			try {
+				delete e[_prop_name];
+				_Object_defineProperty( e, _prop_name, { writable: true, "value": localDict[_prop_name] } );
+			}
+			catch(e) {
+				//Some properties is read-only
+			}
+			
+		}
+	}
+	
+	return e;
+}
+ 
+//export
+global.crossBrowser_initKeyboardEvent = crossBrowser_initKeyboardEvent;
+ 
+})(window);
+
+
 $(window).ready(function () { 
 
 
@@ -52,19 +229,125 @@ $(window).ready(function () {
             return ( e.parentElement ? path(e.parentElement,true) : "window") + "." + n;
         }
 
-        var dispatchMouseEvent = function (target) {
-            var e = document.createEvent("MouseEvents");
-            // If you need clientX, clientY, etc., you can call
-            // initMouseEvent instead of initEvent
-            e.initEvent.apply(e, Array.prototype.slice.call(arguments, 1));
-            target.dispatchEvent(e);
+        var dispatchTextEvent = function (target, type, data) {
+            var t = document.createEvent("TextEvent");
+            t.initTextEvent("textInput", true, true, document.defaultView, data);
+            target.dispatchEvent(t);
+        };
+
+        var modifiers = { altKey: "Alt", ctrlKey: "Control", shiftKey: "Shift", metaKey: "Meta" };
+        function getModifiers(it) {
+            var l = [];
+            for (var i in modifiers) {
+                if (it[i]) {
+                    l.push(modifiers[i]);
+                }
+            }
+            return l.join(" ");
+        }
+
+        var dispatchKeyboardEvent = function (target, type, item) {
+            var oEvent = target.ownerDocument.createEvent('KeyboardEvent');
+            var options = item || {};
+
+
+            //Set your default options to the right of ||
+            var opts = {
+                type: type,
+                canBubble: options.canBubble || true,
+                cancelable: options.cancelable || true,
+                view: options.view || target.ownerDocument.defaultView,
+                key: (options.key || options.which) || 0,
+                location: options.location || 0, //The coordinates within the entire page
+                modifierList: getModifiers(options) || "",
+                repeat: options.repeat || 0, //The coordinates within the viewport
+                locale: options.locale || null,
+            };
+
+            // Chromium Hack
+            Object.defineProperty(oEvent, 'keyCode', {
+                get: function () {
+                    return this.keyIdentifier;
+                }
+            });
+            Object.defineProperty(oEvent, 'which', {
+                get: function () {
+                    return this.keyIdentifier;
+                }
+            });
+
+            //Pass in the options
+            oEvent.initKeyboardEvent(
+                opts.type,
+                opts.canBubble,
+                opts.cancelable,
+                opts.view,
+                opts.key,
+                opts.location,
+                opts.modifierList,
+                opts.repeat,
+                opts.locale
+            );
+
+            //Fire the event
+            target.dispatchEvent(oEvent);
+        };
+
+        var dispatchMouseEvent = function (target, type, o) {
+
+            var event = target.ownerDocument.createEvent('MouseEvents'),
+                options = o || {};
+
+            //Set your default options to the right of ||
+            var opts = {
+                type: type || 'click',
+                canBubble: options.canBubble || true,
+                cancelable: options.cancelable || true,
+                view: options.view || target.ownerDocument.defaultView,
+                detail: options.detail || 1,
+                screenX: options.screenX || 0, //The coordinates within the entire page
+                screenY: options.screenY || 0,
+                clientX: options.clientX || 0, //The coordinates within the viewport
+                clientY: options.clientY || 0,
+                ctrlKey: options.ctrlKey || false,
+                altKey: options.altKey || false,
+                shiftKey: options.shiftKey || false,
+                metaKey: options.metaKey || false, //I *think* 'meta' is 'Cmd/Apple' on Mac, and 'Windows key' on Win. Not sure, though!
+                button: options.button || 0, //0 = left, 1 = middle, 2 = right
+                relatedTarget: options.relatedTarget || null,
+            };
+
+            //Pass in the options
+            event.initMouseEvent(
+                opts.type,
+                opts.canBubble,
+                opts.cancelable,
+                opts.view,
+                opts.detail,
+                opts.screenX,
+                opts.screenY,
+                opts.clientX,
+                opts.clientY,
+                opts.ctrlKey,
+                opts.altKey,
+                opts.shiftKey,
+                opts.metaKey,
+                opts.button,
+                opts.relatedTarget
+            );
+
+            //Fire the event
+            target.dispatchEvent(event);
         };
 
         var defaultActions = {
+            alert: function () {
+                throw new Error("Unexpected alert !!!");
+            },
             type: function (r, e, item) {
                 var a = item.actions;
                 var s = r.steps;
-                for (var i = 0; i < a.length; i++) {
+                for (var i = a.length-1; i >=0; i--) {
                     var ai = a[i];
                     ai.path = item.path;
                     s.unshift(ai);
@@ -85,31 +368,28 @@ $(window).ready(function () {
                 }
             },
             keydown: function (r, e, item) {
-                var evt = $.Event('keydown');
-                copyKeyProperties(item, evt);
-                $(e).trigger(evt);
+                dispatchKeyboardEvent(e, "keydown", item);
             },
             keyup: function(r,e,item){
-                var evt = $.Event('keyup');
-                copyKeyProperties(item, evt);
-                $(e).trigger(evt);
+                dispatchKeyboardEvent(e, "keyup", item);
             },
             keypress: function (r, e, item) {
-                var evt = $.Event('keypress');
-                copyKeyProperties(item, evt);
-                $(e).trigger(evt);
+                dispatchKeyboardEvent(e, "keypress", item);
             },
-            mouseup: function (r, e) {
-                dispatchMouseEvent(e, 'mouseup',  true, true);
+            textInput: function (r, e, item) {
+                dispatchTextEvent(e, "textInput", item.data);
             },
-            mousemove: function (r, e) {
-                dispatchMouseEvent(e, 'mouseover', true, true);
+            mouseup: function (r, e, item) {
+                dispatchMouseEvent(e, 'mouseup',  item);
             },
-            mousedown: function (r, e) {
-                dispatchMouseEvent(e, 'mousedown', true, true);
+            mousemove: function (r, e, item) {
+                dispatchMouseEvent(e, 'mouseover', item);
             },
-            click: function (r, e) {
-                dispatchMouseEvent(e, 'click', true, true);
+            mousedown: function (r, e, item) {
+                dispatchMouseEvent(e, 'mousedown', item);
+            },
+            click: function (r, e, item) {
+                dispatchMouseEvent(e, 'click', item);
             },
             verifyText: function (r, e, item) {
                 var et = $(e).text();
@@ -148,10 +428,16 @@ $(window).ready(function () {
             window.alert = function (msg) {
                 var rec = self;
                 if (rec.state === "recording") {
-                    rec.recordStep({ target: window }, {
-                        action: "alert",
-                        text: msg
-                    });
+
+                    // Since mouse click is not yet recorded
+                    // this is the reason, we will wait for mouse click
+                    // event to be finished first
+                    setTimeout(function () {
+                        rec.recordStep({ target: window }, {
+                            action: "alert",
+                            text: msg
+                        });
+                    }, 1);
                     return windowAlert(msg);
                 }
                 if (rec.state === "running") {
@@ -198,6 +484,12 @@ $(window).ready(function () {
                 });
             };
 
+            this.textInputHandler = function (e) {
+                self.recordStep(e.originalEvent, {
+                    action: "textInput"
+                });
+            };
+
             this.mouseDownHandler = (function (s) {
                 var target = null;
                 var points = [];
@@ -227,7 +519,7 @@ $(window).ready(function () {
 
         };
 
-        var keyProperties = ["which", "altKey", "shiftKey", "ctrlKey", "char", "charCode", "key", "keyCode"];
+        var keyProperties = ["which", "altKey", "shiftKey", "ctrlKey", "char", "charCode", "key", "keyCode", "data", "repeat", "locale", "location"];
 
         var copyKeyProperties = function (src, dest) {
             $.each(keyProperties, function (i,k) {
@@ -249,23 +541,14 @@ $(window).ready(function () {
                 s.path = path(evt.target);
                 var e = evt.target;
 
-                // if last step was an alert, next click should be ignored as 
-                // click will be recorded on alert window, and there is no way
-                // to simulate click on alert window. So alert function will be
-                // just called to confirm that alert was executed successfully but
-                // alert will not be displayed while running
-                if (/alert/i.test(lastStep.action) && /click/i.test(s.action)) {
-                    return;
-                }
-
                 // When action is an keyboard action, we want to combine
                 // multiple keyboard events on one HtmlElement to save space and
                 // emulate change event on input/textarea element
-                if (/keyup|keydown|keypress/i.test(s.action)) {
+                if (/keyup|keydown|keypress|textinput/i.test(s.action)) {
                     copyKeyProperties(evt, s);
                     
                     // check if last step was also a keyboard event
-                    if (/keyup|keydown|keypress|type/i.test(lastStep.action)) {
+                    if (/keyup|keydown|keypress|textinput|type/i.test(lastStep.action)) {
 
                         // only if the element is same, we should combine 
                         // text type event
@@ -292,7 +575,7 @@ $(window).ready(function () {
 
                 this.steps.push(s);
                 this.lastStep = s;
-                if (!/type|keyup|keydown|keypress/i.test(s.action)) {
+                if (!/type|keyup|keydown|keypress|textinput/i.test(s.action)) {
                     this.fireEvent(this);
                 }
             },
@@ -304,6 +587,7 @@ $(window).ready(function () {
                 $(window).keydown(this.keyDownHandler);
                 $(window).keydown(this.keyPressHandler);
                 $(window).mousedown(this.mouseDownHandler);
+                $(window).bind("textInput", this.textInputHandler);
                 this.state = "recording";
                 this.fireEvent(this);
             },
@@ -314,6 +598,7 @@ $(window).ready(function () {
                 $(window).unbind("keypress", this.keyPressHandler);
                 $(window).unbind("keydown", this.keyDownHandler);
                 $(window).unbind("mousedown", this.mouseDownHandler);
+                $(window).unbind("textInput", this.textInputHandler);
                 this.state = "ready";
                 this.fireEvent(this);
             },
--- web/test/test.js
+++ web/test/test.js
@@ -1,8 +1,8 @@
 ﻿[
  {
      "action": "click",
-     "pageX": 139,
-     "pageY": 130,
+     "clientX": 130,
+     "clientY": 142,
      "text": "",
      "path": "#t"
  },
@@ -23,9 +23,8 @@
           "keyCode": 65
       },
       {
-          "action": "keyup",
-          "which": 65,
-          "keyCode": 65
+          "action": "textInput",
+          "data": "a"
       },
       {
           "action": "keydown",
@@ -38,6 +37,15 @@
           "keyCode": 75
       },
       {
+          "action": "textInput",
+          "data": "k"
+      },
+      {
+          "action": "keyup",
+          "which": 65,
+          "keyCode": 65
+      },
+      {
           "action": "keyup",
           "which": 75,
           "keyCode": 75
@@ -51,6 +59,10 @@
           "action": "keypress",
           "which": 65,
           "keyCode": 65
+      },
+      {
+          "action": "textInput",
+          "data": "a"
       },
       {
           "action": "keydown",
@@ -63,9 +75,27 @@
           "keyCode": 83
       },
       {
+          "action": "textInput",
+          "data": "s"
+      },
+      {
           "action": "keyup",
           "which": 65,
           "keyCode": 65
+      },
+      {
+          "action": "keydown",
+          "which": 72,
+          "keyCode": 72
+      },
+      {
+          "action": "keypress",
+          "which": 72,
+          "keyCode": 72
+      },
+      {
+          "action": "textInput",
+          "data": "h"
       },
       {
           "action": "keyup",
@@ -73,43 +103,84 @@
           "keyCode": 83
       },
       {
-          "action": "keydown",
-          "which": 72,
-          "keyCode": 72
-      },
-      {
-          "action": "keypress",
-          "which": 72,
-          "keyCode": 72
-      },
-      {
           "action": "keyup",
           "which": 72,
           "keyCode": 72
       },
       {
           "action": "keydown",
-          "which": 9,
-          "keyCode": 9
+          "which": 8,
+          "keyCode": 8
       },
       {
           "action": "keypress",
-          "which": 9,
-          "keyCode": 9
-      }
-     ],
-     "value": "akash"
- },
- {
-     "action": "type",
-     "path": "body.1.2.0",
-     "which": 9,
-     "keyCode": 9,
-     "actions": [
+          "which": 8,
+          "keyCode": 8
+      },
       {
           "action": "keyup",
-          "which": 9,
-          "keyCode": 9
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keydown",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keypress",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keyup",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keydown",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keypress",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keyup",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keydown",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keypress",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keyup",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keydown",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keypress",
+          "which": 8,
+          "keyCode": 8
+      },
+      {
+          "action": "keyup",
+          "which": 8,
+          "keyCode": 8
       },
       {
           "action": "keydown",
@@ -120,6 +191,10 @@
           "action": "keypress",
           "which": 75,
           "keyCode": 75
+      },
+      {
+          "action": "textInput",
+          "data": "k"
       },
       {
           "action": "keyup",
@@ -133,6 +208,15 @@
       },
       {
           "action": "keypress",
+          "which": 65,
+          "keyCode": 65
+      },
+      {
+          "action": "textInput",
+          "data": "a"
+      },
+      {
+          "action": "keyup",
           "which": 65,
           "keyCode": 65
       },
@@ -147,9 +231,8 @@
           "keyCode": 86
       },
       {
-          "action": "keyup",
-          "which": 65,
-          "keyCode": 65
+          "action": "textInput",
+          "data": "v"
       },
       {
           "action": "keydown",
@@ -160,6 +243,10 @@
           "action": "keypress",
           "which": 65,
           "keyCode": 65
+      },
+      {
+          "action": "textInput",
+          "data": "a"
       },
       {
           "action": "keyup",
@@ -185,8 +272,189 @@
      "value": "kava"
  },
  {
-     "action": "click",
+     "action": "type",
+     "path": "body.1.2.0",
+     "which": 9,
+     "keyCode": 9,
+     "actions": [
+      {
+          "action": "keyup",
+          "which": 9,
+          "keyCode": 9
+      },
+      {
+          "action": "keydown",
+          "which": 65,
+          "keyCode": 65
+      },
+      {
+          "action": "keypress",
+          "which": 65,
+          "keyCode": 65
+      },
+      {
+          "action": "textInput",
+          "data": "a"
+      },
+      {
+          "action": "keydown",
+          "which": 66,
+          "keyCode": 66
+      },
+      {
+          "action": "keypress",
+          "which": 66,
+          "keyCode": 66
+      },
+      {
+          "action": "textInput",
+          "data": "b"
+      },
+      {
+          "action": "keyup",
+          "which": 65,
+          "keyCode": 65
+      },
+      {
+          "action": "keyup",
+          "which": 66,
+          "keyCode": 66
+      },
+      {
+          "action": "keydown",
+          "which": 67,
+          "keyCode": 67
+      },
+      {
+          "action": "keypress",
+          "which": 67,
+          "keyCode": 67
+      },
+      {
+          "action": "textInput",
+          "data": "c"
+      },
+      {
+          "action": "keydown",
+          "which": 68,
+          "keyCode": 68
+      },
+      {
+          "action": "keypress",
+          "which": 68,
+          "keyCode": 68
+      },
+      {
+          "action": "textInput",
+          "data": "d"
+      },
+      {
+          "action": "keyup",
+          "which": 67,
+          "keyCode": 67
+      },
+      {
+          "action": "keyup",
+          "which": 68,
+          "keyCode": 68
+      },
+      {
+          "action": "keydown",
+          "which": 49,
+          "keyCode": 49
+      },
+      {
+          "action": "keypress",
+          "which": 49,
+          "keyCode": 49
+      },
+      {
+          "action": "textInput",
+          "data": "1"
+      },
+      {
+          "action": "keyup",
+          "which": 49,
+          "keyCode": 49
+      },
+      {
+          "action": "keydown",
+          "which": 50,
+          "keyCode": 50
+      },
+      {
+          "action": "keypress",
+          "which": 50,
+          "keyCode": 50
+      },
+      {
+          "action": "textInput",
+          "data": "2"
+      },
+      {
+          "action": "keyup",
+          "which": 50,
+          "keyCode": 50
+      },
+      {
+          "action": "keydown",
+          "which": 51,
+          "keyCode": 51
+      },
+      {
+          "action": "keypress",
+          "which": 51,
+          "keyCode": 51
+      },
+      {
+          "action": "textInput",
+          "data": "3"
+      },
+      {
+          "action": "keyup",
+          "which": 51,
+          "keyCode": 51
+      },
+      {
+          "action": "keydown",
+          "which": 9,
+          "keyCode": 9
+      },
+      {
+          "action": "keypress",
+          "which": 9,
+          "keyCode": 9
+      }
+     ],
+     "value": "abcd123"
+ },
+ {
+     "action": "type",
      "path": "body.1.3.0",
+     "which": 9,
+     "keyCode": 9,
+     "actions": [
+      {
+          "action": "keyup",
+          "which": 9,
+          "keyCode": 9
+      },
+      {
+          "action": "keydown",
+          "which": 32,
+          "keyCode": 32
+      },
+      {
+          "action": "keypress",
+          "which": 32,
+          "keyCode": 32
+      },
+      {
+          "action": "keyup",
+          "which": 32,
+          "keyCode": 32
+      }
+     ],
      "value": "Click Me"
  },
  {
